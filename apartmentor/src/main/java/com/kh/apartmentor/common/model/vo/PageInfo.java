325cc787cdc9ec6dc5499b32773d68fd2b0a78e6
haveway <haveway@gmail.com>
2022-08-03 12:44:51 +0900

    페이지처리 수정
    페이지처리수정

--- apartmentor/src/main/java/com/kh/apartmentor/common/model/vo/PageInfo.java
+++ apartmentor/src/main/java/com/kh/apartmentor/common/model/vo/PageInfo.java
@@ -1,4 +1,4 @@
-package com.kh.spring.common.model.vo;
+package com.kh.apartmentor.common.model.vo;
 
 public class PageInfo {
 	
--- apartmentor/src/main/java/com/kh/apartmentor/common/template/Pagination.java
+++ apartmentor/src/main/java/com/kh/apartmentor/common/template/Pagination.java
@@ -1,6 +1,6 @@
-package com.kh.spring.common.template;
+package com.kh.apartmentor.common.template;
 
-import com.kh.spring.common.model.vo.PageInfo;
+import com.kh.apartmentor.common.model.vo.PageInfo;
 
 public class Pagination {
 	
