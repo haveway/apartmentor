95ab588ff08a4cca4ae078b1da6c452da20ef58e
DonghwanSon1 <110151129+DonghwanSon1@users.noreply.github.com>
2022-08-05 17:19:33 +0900

    config 넣었습니다. -동환

--- apartmentor/src/main/java/com/kh/apartmentor/sports/controller/SportsController.java
+++ apartmentor/src/main/java/com/kh/apartmentor/sports/controller/SportsController.java
@@ -1,16 +1,43 @@
 package com.kh.apartmentor.sports.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.apartmentor.board.model.service.BoardService;
+import com.kh.apartmentor.common.model.vo.Reserve;
+import com.kh.apartmentor.sports.service.SportsService;
+
 @Controller
 public class SportsController {
+	
+	@Autowired
+	private SportsService sportsService;
 
 	@RequestMapping("golf.sp")
-	public String reserve() {
+	public String reserveGolf() {
+		return "sports/golf";
+	}
+	
+	
+	@RequestMapping("golfSeatList.sp")
+	public String reserve(Reserve r) {
+		//System.out.println(r.getStartDate());
+		//System.out.println(r.getStartDay());
+		
+		sportsService.selectGolfSeatList(r);
+		
 		
 		return "sports/golf";
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	@RequestMapping("miniGym.sp")
--- apartmentor/src/main/java/com/kh/apartmentor/sports/dao/SportsDao.java
+++ apartmentor/src/main/java/com/kh/apartmentor/sports/dao/SportsDao.java
@@ -1,5 +1,35 @@
 package com.kh.apartmentor.sports.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.apartmentor.common.model.vo.Reserve;
+
+@Repository
 public class SportsDao {
 
+	
+	public Reserve selectGolfSeatList(SqlSessionTemplate sqlSession, Reserve r) {
+		return sqlSession.selectOne("boardMapper.selectBoard", r);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
